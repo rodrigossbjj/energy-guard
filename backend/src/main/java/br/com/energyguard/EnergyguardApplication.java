288470c8c929f1e967ab2a260b54def939a2c581
Rodrigo Sousa <rodrigosousasales9@gmail.com>
2026-08-22 14:12:48 -0300

    chore: start project

---
+++ backend/src/main/java/br/com/energyguard/EnergyguardApplication.java
@@ -0,0 +1,13 @@
+package br.com.energyguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyguardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/energyguard/EnergyguardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.energyguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
